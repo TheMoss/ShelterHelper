e444f1ed7570ef3b6e4de88927daf199329b5e7c
TheMoss <milena.k98@gmail.com>
2024-01-16 16:50:08 +0100

    Add validation attributes to Animal model.

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -2,22 +2,37 @@
 
 namespace ShelterHelper.Models
 {
-	public class Animal
-	{
-		public int Id { get; set; }
-		public string Species { get; set; }
-		public string Name { get; set; }
-		public string Sex { get; set; }
-		public int Weight { get; set; }
-		[Display(Name = "Admission day")]
-		[DataType(DataType.Date)]
-		public DateTime AdmissionDay { get; set; }
-		[Display(Name = "Adoption day")]
-		[DataType(DataType.Date)]
-		public DateTime AdoptionDay { get; set; }
-		public string Health { get; set; }
-		[Display(Name = "Employee ID")]
-		public int EmployeeId { get; set; }
+    public class Animal
+    {
+        public int Id { get; set; }
 
-	}
+        [Required]
+        public string Species { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        [Required]
+        public int Weight { get; set; }
+
+        [Required]
+        [Display(Name = "Admission day")]
+        [DataType(DataType.Date)]
+        public DateTime AdmissionDay { get; set; }
+
+        [Display(Name = "Adoption day")]
+        [DataType(DataType.Date)]
+        public DateTime AdoptionDay { get; set; }
+
+        [Required]
+        public string Health { get; set; }
+
+        [Required]
+        [Display(Name = "Employee ID")]        
+        public int EmployeeId { get; set; }
+    }
+
 }

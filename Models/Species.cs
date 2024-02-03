14fffe4d209b1c964ded5c6e817a0a69329bcfc8
TheMoss <milena.k98@gmail.com>
2024-02-03 22:52:39 +0100

    feat: add database context class and a model class for Species, Toy, Accessory, Diet, Bedding tables

---
+++ Models/Species.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelper.Models
+{
+	public class Species
+	{
+		[Key]
+		public int SpeciesId { get; set; }
+        [Display(Name = "Species")]
+        public string SpeciesName { get; set; }
+		public int DietId { get; set; }
+		public virtual Diet Diet { get; set; }
+		public int BeddingId { get; set; }
+		public virtual Bedding Bedding { get; set; }
+		public int ToyId { get; set; }
+		public virtual Toy Toy { get; set; }
+		public int AccessoryId { get; set; }
+		public virtual Accessory Accessory { get; set; }
+	}
+
+
+
+	public class Diet
+	{
+		[Key]
+		public int DietId { get; set; }
+        [Display(Name = "Diet")]
+        public string DietName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity_kg { get; set; }
+	}
+
+	public class Bedding
+	{
+		[Key]
+		public int BeddingId { get; set; }
+        [Display(Name = "Bedding")]
+        public string BeddingName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity_kg { get; set; }
+	}
+
+	public class Toy
+	{
+		[Key]
+		public int ToyId { get; set; }
+        [Display(Name = "Toy")]
+        public string ToyName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+	}
+
+	public class Accessory
+	{
+		[Key]
+		public int AccessoryId { get; set; }
+        [Display(Name = "Accessory")]
+        public string AccessoryName { get; set; }
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+	}
+}

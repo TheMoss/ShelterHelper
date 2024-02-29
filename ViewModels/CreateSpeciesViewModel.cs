796924785a37abf3c3855b59b0eab46c48d08198
TheMoss <milena.k98@gmail.com>
2024-02-29 14:42:34 +0100

    refactor: add a viewmodel for storing database's tables contents for dropdown lists in creation form so multiple models can be included.

---
+++ ViewModels/CreateSpeciesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ShelterHelper.Models;
+namespace ShelterHelper.ViewModels
+{
+    public class CreateSpeciesViewModel
+    {
+        public string SpeciesName { get; set; }
+        public int SelectedDietId { get; set; }
+        public List<Diet>? DietsList { get; set; }
+        public int SelectedBeddingId { get; set; }
+        public List<Bedding>? BeddingsList { get; set; }
+        public int SelectedToyId { get; set; }
+        public List<Toy>? ToysList { get; set; }
+        public int SelectedAccessoryId { get; set; }
+        public List<Accessory>? AccessoriesList { get; set; }
+    }
+}

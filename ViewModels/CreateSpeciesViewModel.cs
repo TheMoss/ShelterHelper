a08c625e2ebaa338927f8d7bb01bc1ef6e9c1b94
TheMoss <milena.k98@gmail.com>
2024-03-11 13:43:05 +0100

    feat: add references to classes so their properties can be accessed.

--- ViewModels/CreateSpeciesViewModel.cs
+++ ViewModels/CreateSpeciesViewModel.cs
@@ -3,14 +3,18 @@ namespace ShelterHelper.ViewModels
 {
     public class CreateSpeciesViewModel
     {
-        public string SpeciesName { get; set; }
+        public string? SpeciesName { get; set; }
         public int SelectedDietId { get; set; }
+        public Diet? Diet { get; set; }
         public List<Diet>? DietsList { get; set; }
         public int SelectedBeddingId { get; set; }
+        public Bedding? Bedding { get; set; }
         public List<Bedding>? BeddingsList { get; set; }
         public int SelectedToyId { get; set; }
+        public Toy? Toy { get; set; }
         public List<Toy>? ToysList { get; set; }
         public int SelectedAccessoryId { get; set; }
+        public Accessory? Accessory { get; set; }
         public List<Accessory>? AccessoriesList { get; set; }
     }
 }

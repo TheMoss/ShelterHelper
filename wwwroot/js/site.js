6fda0b4fb40ce74817dc9002f551d7d22faeb6b8
TheMoss <milena.k98@gmail.com>
2024-04-17 15:34:13 +0200

    feat: add scripts for switching tabs and adding and deleting rows in AddResources form.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,10 +5,49 @@
 
 
 
-function closeModal(selector) {	
-	$(selector).modal("hide");
+function closeModal(selector) {
+    $(selector).modal("hide");
 }
 
 function openModal(selector) {
-	$(selector).modal("show");
+    $(selector).modal("show");
 }
+document.getElementById('openDefault').click();
+function openType(event, typeName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+
+    }
+
+    document.getElementById(typeName).style.display = "block";
+    event.currentTarget.className += "active";
+}
+
+function cloneRow(rowsId) {
+    var row = document.getElementById(rowsId).firstElementChild;
+    var clone = row.cloneNode(true);
+
+    clone.prepend(document.createElement("hr"));
+
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn', 'btn-danger', 'deleteBtn');
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener('click', deleteRow);
+    clone.appendChild(deleteButton);
+
+    document.querySelector('input').value = "";
+    document.getElementById(rowsId).appendChild(clone);
+
+    function deleteRow() {
+        document.getElementById(rowsId).removeChild(clone);
+    }
+};
+

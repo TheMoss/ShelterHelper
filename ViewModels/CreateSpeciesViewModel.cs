ee8256017cba7b68668d92318bd9b5631d4a7826
TheMoss <milena.k98@gmail.com>
2024-05-14 18:37:23 +0200

    refactor: add placeholder messages for IDs.

--- ViewModels/CreateSpeciesViewModel.cs
+++ ViewModels/CreateSpeciesViewModel.cs
@@ -1,19 +1,28 @@
 ﻿using ShelterHelper.Models;
+using System.ComponentModel.DataAnnotations;
 namespace ShelterHelper.ViewModels
 {
     public class CreateSpeciesViewModel
     {
-        public string? SpeciesName { get; set; }
-        public int SelectedDietId { get; set; }
+        public Species? Species { get; set; }
+
+		[Display(Name = "Choose a diet")]
+		public int SelectedDietId { get; set; }
         public Diet? Diet { get; set; }
         public List<Diet>? DietsList { get; set; }
+		
+        [Display(Name = "Choose a bedding")]
         public int SelectedBeddingId { get; set; }
         public Bedding? Bedding { get; set; }
         public List<Bedding>? BeddingsList { get; set; }
-        public int SelectedToyId { get; set; }
+
+		[Display(Name = "Choose a toy")]
+		public int SelectedToyId { get; set; }
         public Toy? Toy { get; set; }
         public List<Toy>? ToysList { get; set; }
-        public int SelectedAccessoryId { get; set; }
+
+		[Display(Name = "Choose an accessory")]
+		public int SelectedAccessoryId { get; set; }
         public Accessory? Accessory { get; set; }
         public List<Accessory>? AccessoriesList { get; set; }
     }

1a8cdce6b8fbf0e4908583d78fe367fcd4c0fb55
TheMoss <milena.k98@gmail.com>
2024-06-18 22:50:36 +0200

    refactor: remove properties duplicated from referenced models, add a reference to Employee model.

--- ViewModels/AnimalViewModel.cs
+++ ViewModels/AnimalViewModel.cs
@@ -5,36 +5,9 @@ namespace ShelterHelper.ViewModels
 {
 	public class AnimalViewModel
 	{
-		public Animal? Animal { get; set; }
-        [Required(ErrorMessage = "This field is required")]
-        [Display(Name = "Species")]
-        public int SpeciesId { get; set; }
+		public Animal? Animal { get; set; }        
         public virtual Species? Species { get; set; }
-
-        [Required]
-        [StringLength(30, MinimumLength = 3)]
-        public string Name { get; set; }
-        [Required]
-        public string Sex { get; set; }
-
-        [Required]
-        public int Weight { get; set; }
-
-        [Required]
-        [Display(Name = "Admission day")]
-        [DataType(DataType.Date)]
-        public DateOnly AdmissionDay { get; set; }
-
-        [Display(Name = "Adoption day")]
-        [DataType(DataType.Date)]
-        public DateOnly? AdoptionDay { get; set; }
-
-        [Required]
-        public string Health { get; set; }
-
-        [Required]
-        [Display(Name = "Employee ID")]
-        public int EmployeeId { get; set; }
-        public List<Species>? SpeciesList { get; set; }
+		public List<Species>? SpeciesList { get; set; }
+		public Employee? Employee { get; set; }
 	}
 }

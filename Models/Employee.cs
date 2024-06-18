a4bfaa0c41f908f544980a92ec7a4bdfe5de2f5e
TheMoss <milena.k98@gmail.com>
2024-06-18 22:49:16 +0200

    feat: add Employee model.

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelper.Models
+{
+	public class Employee
+	{
+		[Key]
+		public int EmployeeId { get; set; }
+		[Display(Name = "Employee ID")]
+		[Length(minimumLength: 6, maximumLength: 6)]
+		public int EmployeePersonalId{ get; set; }
+		public string EmployeeName { get; set; }
+
+	}
+}

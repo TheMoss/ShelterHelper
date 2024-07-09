7e7f28d6538551306788a73907428b9cd4e0a728
TheMoss <milena.k98@gmail.com>
2024-07-09 23:03:17 +0200

    style: group attributes in one bracket to improve readability

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -2,45 +2,42 @@
 
 namespace ShelterHelper.Models
 {
-    public class Animal
-    {
-        [Key]
-        public int? Id { get; set; }
-		[Required(ErrorMessage = "This field is required")]
-		[Display(Name = "Species")]
+	public class Animal
+	{
+		[Key]
+		public int? Id { get; set; }
+
+		[Display(Name = "Species"), Required(ErrorMessage = "This field is required")]
 		public int SpeciesId { get; set; }
-        public virtual Species? Species { get; set; }
-        [Display(Name = "Chip number")]
-        //[Length(minimumLength: 8, maximumLength: 8)]
-        [Range(10000000, 99999999)]
-        public int ChipNumber { get; set; }
 
-        [Required]
-        [StringLength(30, MinimumLength = 3)]
-        public string Name { get; set; }
+		public virtual Species? Species { get; set; }
 
-        [Required]
-        public string Sex { get; set; }
+		[Display(Name = "Chip number"), Range(10000000, 99999999), Required]
+		public int ChipNumber { get; set; }
 
-        [Required]
-        public int Weight { get; set; }
+		[StringLength(30, MinimumLength = 3), Required]
+		public string Name { get; set; }
 
-        [Required]
-        [Display(Name = "Admission day")]
-        [DataType(DataType.Date)]
-        public DateOnly AdmissionDay { get; set; }
+		public string? Sex { get; set; }
 
-        [Display(Name = "Adoption day")]
-        [DataType(DataType.Date)]
-        public DateOnly? AdoptionDay { get; set; }
+		[Required]
+		public int Weight { get; set; }
 
-        [Required]
-        public string Health { get; set; }
+		[DataType(DataType.Date), Display(Name = "Admission day"), Required]
+		public DateOnly AdmissionDay { get; set; }
 
-        public virtual Employee? Employee { get; set; }
-        public int EmployeeId { get; set; }
-        public int EmployeePersonalId {  get; set; }
-        
-    }
+		[DataType(DataType.Date), Display(Name = "Adoption day")]
+		public DateOnly? AdoptionDay { get; set; }
+
+		public string? Health { get; set; }
+
+		public virtual Employee? Employee { get; set; }
+		public int EmployeeId { get; set; }
+		public int EmployeePersonalId { get; set; }
+
+		public virtual Owner? Owner { get; set; }
+		public int? OwnerId { get; set; }
+
+	}
 
 }

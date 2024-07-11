2adcb582d985ec046dcf3d56270e5d1eb9729767
TheMoss <milena.k98@gmail.com>
2024-07-11 15:17:09 +0200

    feat: create model for Owner

---
+++ Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelper.Models
+{
+	public class Owner
+	{
+		public int OwnerId { get; set; }
+		[Display(Name = "Owner's name"), Required]		
+		public string OwnerName { get; set; }
+		[Required]
+		public string Address { get; set; }
+		[Required]
+		public string Email { get; set; }
+	}
+}

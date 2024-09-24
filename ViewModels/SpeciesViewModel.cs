352cb12e4d5e82e970e598c9e8152ee8d2c1cee8
TheMoss <milena.k98@gmail.com>
2024-09-24 18:12:53 +0200

    refactor: declare selected item IDs as nullable
    Declaring as nullable allows to omit them when a method doesn't need these values

--- ViewModels/SpeciesViewModel.cs
+++ ViewModels/SpeciesViewModel.cs
@@ -4,25 +4,27 @@ namespace ShelterHelper.ViewModels
 {
     public class SpeciesViewModel
     {
-        public Species? Species { get; set; }
+		public int? SpeciesId { get; set; }
+        public string? SpeciesName { get;set; }
+		public Species? Species { get; set; }
 
 		[Display(Name = "Choose a diet")]
-		public int SelectedDietId { get; set; }
+		public int? SelectedDietId { get; set; }
         public Diet? Diet { get; set; }
         public List<Diet>? DietsList { get; set; }
 		
         [Display(Name = "Choose a bedding")]
-        public int SelectedBeddingId { get; set; }
+        public int? SelectedBeddingId { get; set; }
         public Bedding? Bedding { get; set; }
         public List<Bedding>? BeddingsList { get; set; }
 
 		[Display(Name = "Choose a toy")]
-		public int SelectedToyId { get; set; }
+		public int? SelectedToyId { get; set; }
         public Toy? Toy { get; set; }
         public List<Toy>? ToysList { get; set; }
 
 		[Display(Name = "Choose an accessory")]
-		public int SelectedAccessoryId { get; set; }
+		public int? SelectedAccessoryId { get; set; }
         public Accessory? Accessory { get; set; }
         public List<Accessory>? AccessoriesList { get; set; }
     }

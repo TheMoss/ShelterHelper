b9bf5333344d7d860c07906b8bf2be186248dbd6
TheMoss <milena.k98@gmail.com>
2024-10-17 18:18:56 +0200

    feat: add assignment model and controller

---
+++ Controllers/AssignmentsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShelterHelper.Models;
+
+namespace ShelterHelper.Controllers
+{
+	public class AssignmentsController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IHttpClientFactory _httpClientFactory;
+
+
+		public AssignmentsController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+		{
+			_logger = logger;
+			_httpClientFactory = httpClientFactory;
+
+		}
+		// GET: AssignmentsController
+		public async Task<IActionResult> Index()
+		{
+			IEnumerable<Assignment> assignments = null;
+			var httpClient = _httpClientFactory.CreateClient("ShelterHelperAPI");
+			var response = await httpClient.GetAsync("api/assignments");
+			
+			if (response.IsSuccessStatusCode)
+			{
+				assignments = await response.Content.ReadAsAsync<IEnumerable<Assignment>>();
+			}
+			return View(assignments);
+		}
+		// GET: AssignmentsController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: AssignmentsController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: AssignmentsController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: AssignmentsController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: AssignmentsController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: AssignmentsController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ Models/Assignment.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelper.Models
+{
+	public class Assignment
+	{
+		public int? AssignmentId { get; set; }
+		[Required]
+		[StringLength(100)]
+		public string Title { get; set; }
+		[StringLength(300)]
+		public string? Description { get; set; }
+		[Range(0,3)]
+		public int Priority { get; set; }
+		[Display(Name = "Creator's ID")]
+		public int CreatorId {  get; set; }
+		[Display(Name = "Creation date")]
+		public DateOnly? CreationDate { get; set; }
+		public bool? IsCompleted { get; set; } = false;
+		public bool? IsInProgress { get; set; } = false;
+
+	}
+}

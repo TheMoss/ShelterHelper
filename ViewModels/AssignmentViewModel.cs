296497240ce1b9d2a81b63d3612830231265fd30
TheMoss <milena.k98@gmail.com>
2024-11-22 21:52:58 +0100

    Adds an AssignmentViewModel

---
+++ ViewModels/AssignmentViewModel.cs
@@ -0,0 +1,11 @@
+using ShelterHelper.Models;
+
+namespace ShelterHelper.ViewModels;
+
+public class AssignmentViewModel
+{
+    public Assignment Assignment { get; set; }
+    public Employee Employee { get; set; }
+    
+    public List<Employee>? EmployeesList { get; set; } 
+}

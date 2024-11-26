9884297c026591c52254c91893ddab852495f64e
TheMoss <milena.k98@gmail.com>
2024-11-26 18:33:56 +0100

    Moves the employee multiselect list and script from Create to Edit view
    An assignment's ID is needed to assign employees to it and only an already existing record has an ID

---
+++ Models/EmployeesAssignments.cs
@@ -0,0 +1,16 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ShelterHelper.Models;
+
+public class EmployeesAssignments
+{
+    [Key]
+    public int? Id { get; set; }
+    
+    public int EmployeeId { get; set; }
+    public int AssignmentId { get; set; }
+
+    public Employee Employee { get; set; }
+    public Assignment Assignment { get; set; }
+}

e255fddf07e7570c47dd93e3f8e8164d70dbbd49
TheMoss <milena.k98@gmail.com>
2024-11-26 18:36:09 +0100

    Adds a list to store selected emplopyees' database id numbers
    The employees' database id numbers will be needed to connect them to the specific assignment

--- ViewModels/AssignmentViewModel.cs
+++ ViewModels/AssignmentViewModel.cs
@@ -4,8 +4,9 @@ namespace ShelterHelper.ViewModels;
 
 public class AssignmentViewModel
 {
-    public Assignment Assignment { get; set; }
-    public Employee Employee { get; set; }
-    
-    public List<Employee>? EmployeesList { get; set; } 
+    public Assignment? Assignment { get; set; }
+    public Employee? Employee { get; set; }
+
+    public List<Employee>? EmployeesList { get; set; } = new();
+    public List<int>? SelectedEmployeesIds { get; set; }
 }

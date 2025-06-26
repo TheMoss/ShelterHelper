b4660b97d25057dd0b308bd4657bb139edba270a
TheMoss <milena.k98@gmail.com>
2025-06-26 14:24:13 +0200

    Add entities to the context and configure the options builder

--- Models/ShelterContext.cs
+++ Models/ShelterContext.cs
@@ -4,11 +4,16 @@ namespace ShelterHelper.Models
 {
     public class ShelterContext : DbContext
     {
+        public DbSet<Accessory> Accessory { get; set; }
         public DbSet<Animal> AnimalsDb { get; set; }
-        public DbSet<Species> SpeciesDb { get; set; }
-        public DbSet<Assignment> AssignmentsDb { get; set; }
-        public DbSet<Employee> EmployeesDb { get; set; }
-        public DbSet<EmployeeAssignment> EmployeesAssignmentsDb { get; set; }
+        public DbSet<Bedding> Bedding { get; set; }
+        public DbSet<Diet> Diet { get; set; }
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Assignment> Assignment { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeAssignment> EmployeesAssignments { get; set; }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<Toy> Toy { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -17,7 +22,7 @@ namespace ShelterHelper.Models
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("ShelterContext")).UseLazyLoadingProxies();
         }
 
         public ShelterContext(DbContextOptions<ShelterContext> options) : base(options)

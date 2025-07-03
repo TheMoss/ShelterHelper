4e74cd0767ba22592761d2f01f31de0743752e63
TheMoss <milena.k98@gmail.com>
2025-07-03 16:43:30 +0200

    Add migrations files

---
+++ Migrations/20250424202845_MakePersonalIdAndNameNullable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using ShelterHelper.Models;
+
+#nullable disable
+
+namespace ShelterHelper.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePersonalIdAndNameNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                table:"Employee",
+                name: "EmployeePersonalId", 
+                nullable:true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.AlterColumn<int>(
+                table:"Employee",
+                name: "EmployeePersonalId", 
+                nullable:false);
+        }
+    }
+}
---
+++ Migrations/20250424204609_CorrectColumnType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShelterHelper.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                table:"Employee",
+                name: "EmployeePersonalId", 
+                nullable:true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.AlterColumn<int>(
+                table:"Employee",
+                name: "EmployeePersonalId", 
+                nullable:false);
+        }
+    }
+}
